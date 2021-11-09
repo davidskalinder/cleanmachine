d8210332b255655e3a49ad97694f4aaacf5933d4
David Skalinder <skalinder@wisc.edu>
2021-11-09 10:30:21 -0600

    Add docs for skeleton-writer

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,23 @@
 # write_metadata_skeleton ----
 
+#' Write a skeleton metadata file
+#'
+#' @param source_vars A data frame with one row per source variable.
+#' @param outfile Destination of the skeleton file.
+#' @param source_varname_col Tidy-select column in \code{source_vars} with the
+#'   name of the variable.
+#' @param num_exprs Number of new/old expression cols to include in file.
+#'
+#' @return Nothing, called only for side effects.
+#' @export
+#'
+#' @examples
+#' write_metadata_skeleton(
+#'   source_vars = tibble::tibble(source_varname = names(gss_demo)),
+#'   outfile = tempfile(),
+#'   source_varname_col = source_varname,
+#'   num_exprs = 5
+#' )
 write_metadata_skeleton <-
   function(
     source_vars,
